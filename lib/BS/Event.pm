2ef21091bc0c65eee17ddf9a1710e210537a4b0f
Robin Redeker <elmex@ta-sa.org>
2008-03-22 13:03:37 +0100

    bumped the version

--- lib/BS/Event.pm
+++ lib/BS/Event.pm
@@ -8,11 +8,11 @@ BS::Event - A class that provides an event callback interface
 
 =head1 VERSION
 
-Version 0.2
+Version 0.3
 
 =cut
 
-our $VERSION = '0.2';
+our $VERSION = '0.3';
 
 =head1 SYNOPSIS
 
