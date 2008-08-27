efbf414fdbe7472601e560d0b53c55ac8632af34
Robin Redeker <elmex@ta-sa.org>
2008-08-27 19:05:39 +0200

    bumped version

--- lib/Object/Event.pm
+++ lib/Object/Event.pm
@@ -8,11 +8,11 @@ Object::Event - A class that provides an event callback interface
 
 =head1 VERSION
 
-Version 0.4
+Version 0.5
 
 =cut
 
-our $VERSION = '0.4';
+our $VERSION = '0.5';
 
 =head1 SYNOPSIS
 
